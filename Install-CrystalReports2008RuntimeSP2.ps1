dbf708d891e4fdc7de44452adce95a56e0ac00dd
Johnny Radeck <johnny@radeck.se>
2017-04-21 20:35:22 +0200

    Delete Install-CrystalReports2008RuntimeSP2.ps1

--- Install-CrystalReports2008RuntimeSP2.ps1
+++
@@ -1,7 +0,0 @@
-if (!((Get-WmiObject Win32_Product).name -eq 'Crystal Reports 2008 Runtime SP2')){
-Write-Verbose –Message 'Installing Crystal Reports 2008 Runtime SP2' –Verbose
-start-process 'c:\Windows\System32\msiexec.exe' -ArgumentList "/i CRRuntime_12_2_mlb.msi /QB! REBOOT=ReallySuppress /l*v $env:WINDIR\Temp\Install-Crystal Reports 2008 Runtime SP2.log" -wait -Verbose
-}
-Else{
-Write-Verbose –Message 'Found Crystal Reports 2008 Runtime SP2' –Verbose
-}
