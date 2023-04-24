924d4e30f60666748db06f0bb5773ab86e7b90e1
Johnny Radeck <johnny@radeck.se>
2023-04-24 11:51:38 +0200

    Create Remove Microsoft Store for Business app from Intune.ps1

---
+++ Intune Stuff/Remove Microsoft Store for Business app from Intune.ps1
@@ -0,0 +1,14 @@
+﻿Connect-MSGraph
+
+$Resource = 'deviceAppManagement/mobileApps'
+$StoreForBusinessApp = (Invoke-MSGraphRequest -HttpMethod GET -Url $Resource).Value | Where-Object "@odata.type" -eq "#microsoft.graph.microsoftStoreForBusinessApp"
+
+Foreach ($App in $StoreForBusinessApp) {
+
+    $URI = "deviceAppManagement/mobileApps/$($App.Id)"
+    $App.displayName
+    Invoke-MSGraphRequest -HttpMethod DELETE -Url $URI
+
+}
+
+
